9339d1e29000f62da89ebd114744eea193fa926e
Yizhou Sun <yizhou.sun78@gmail.com>
2025-02-23 22:00:52 -0500

    Update Register.java

--- Assignments/A2/LinkedInSystem/linkedin_system/src/Register.java
+++ Assignments/A2/LinkedInSystem/linkedin_system/src/Register.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -6,6 +7,8 @@ public class Register {
     private Map<Integer, List<User>> connections;
     private Map<Integer, List<User>> followers;
     private Map<Integer, List<User>> following;
+    private Map<Integer, List<User>> connectSent;
+    private Map<Integer, List<User>> connectReceived;
     private Map<Integer, List<Post>> posts;
     private Map<Integer, List<Comment>> myComments; // key: userId - All comments a user made
     private Map<Integer, List<Comment>> commentsReceived; // key: userId - All comments a user received
@@ -13,12 +16,14 @@ public class Register {
     private Map<Integer, List<Like>> likesReceived; // key: userId - All likes a user received from posts and comments
     private Map<Integer, List<Share>> myShares; // key: userId - All posts a user shared
     private Map<Integer, List<Share>> sharesReceived; // key: userId - All shares a user received from connections
-    private Map<Integer, List<NotificationType>> notifications; // key: userId - All notifications a user received
+    private Map<Integer, List<Notification>> notifications; // key: userId - All notifications a user received
 
     public Register() {
         this.connections = new HashMap<Integer, List<User>>();
         this.followers = new HashMap<Integer, List<User>>();
         this.following = new HashMap<Integer, List<User>>();
+        this.connectSent = new HashMap<Integer, List<User>>();
+        this.connectReceived = new HashMap<Integer, List<User>>();
         this.posts = new HashMap<Integer, List<Post>>();
         this.myComments = new HashMap<Integer, List<Comment>>();
         this.commentsReceived = new HashMap<Integer, List<Comment>>();
@@ -26,6 +31,58 @@ public class Register {
         this.likesReceived = new HashMap<Integer, List<Like>>();
         this.myShares = new HashMap<Integer, List<Share>>();
         this.sharesReceived = new HashMap<Integer, List<Share>>();
-        this.notifications = new HashMap<Integer, List<NotificationType>>();
+        this.notifications = new HashMap<Integer, List<Notification>>();
+    }
+
+    public Map<Integer, List<User>> getConnections() {
+        return connections;
+    }
+
+    public Map<Integer, List<User>> getFollowers() {
+        return followers;
+    }
+
+    public Map<Integer, List<User>> getFollowing() {
+        return following;
+    }
+
+    public Map<Integer, List<User>> getConnectSent() {
+        return connectSent;
+    }
+
+    public Map<Integer, List<User>> getConnectReceived() {
+        return connectReceived;
+    }
+
+    public Map<Integer, List<Post>> getPosts() {
+        return posts;
+    }
+
+    public Map<Integer, List<Comment>> getMyComments() {
+        return myComments;
+    }
+
+    public Map<Integer, List<Comment>> getCommentsReceived() {
+        return commentsReceived;
+    }
+
+    public Map<Integer, List<Like>> getMyLikes() {
+        return myLikes;
+    }
+
+    public Map<Integer, List<Like>> getLikesReceived() {
+        return likesReceived;
+    }
+
+    public Map<Integer, List<Share>> getMyShares() {
+        return myShares;
+    }
+
+    public Map<Integer, List<Share>> getSharesReceived() {
+        return sharesReceived;
+    }
+
+    public Map<Integer, List<Notification>> getNotifications() {
+        return notifications;
     }
 }
